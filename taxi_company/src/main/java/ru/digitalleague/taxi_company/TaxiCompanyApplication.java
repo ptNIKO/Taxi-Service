964467e304decff9db5d5dedeeaf7bafd11133c7
ptNIKO <simonich2016@gmail.com>
2021-10-04 15:00:36 +0300

    @feature add new module  taxi_company

---
+++ taxi_company/src/main/java/ru/digitalleague/taxi_company/TaxiCompanyApplication.java
@@ -0,0 +1,13 @@
+package ru.digitalleague.taxi_company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiCompanyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxiCompanyApplication.class, args);
+    }
+
+}
