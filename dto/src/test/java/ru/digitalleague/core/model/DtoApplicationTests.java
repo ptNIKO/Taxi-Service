477f557fb6c87515eb954a8ae98bf8fbc8c18cd6
ptNIKO <simonich2016@gmail.com>
2021-10-15 19:01:21 +0300

    @feature add dto

---
+++ dto/src/test/java/ru/digitalleague/core/model/DtoApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.digitalleague.core.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
